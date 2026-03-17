56ec920786987ee9ecdb1ecd5c79d3e1655fd946
Zeljo88 <your@email.com>
2026-03-17 16:15:19 +0000

    Fix npm path in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 COMPOSER="/var/www/vhosts/rezultati.net/.phpenv/shims/composer"
+NPM="/var/www/vhosts/rezultati.net/.nodenv/shims/npm"
 PHP="/usr/bin/php"
 APP="/var/www/vhosts/rezultati.net/httpdocs"
 
 cd $APP
 $COMPOSER install --no-dev --optimize-autoloader
-npm install && npm run build
+$NPM install
+$NPM run build
 $PHP artisan migrate --force
 $PHP artisan config:cache
 $PHP artisan route:cache
